0e1b333e7476b8d6f575e3c2159fa0dea1a05cd2
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-19 18:45:40 +0900

    Add A Swift Recap Step

---
+++ Scratchpad.playground/section-1.swift
@@ -0,0 +1,24 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+// Swift Recap
+
+var someVariable = "aVariable"
+let someConstant = 20
+
+someVariable = "anotherString"
+
+// var fruitsArray: [String] = ["apples"]
+var fruitsArray = ["apples"]
+fruitsArray.append("bananas")
+
+fruitsArray
+
+// Optional
+
+var optionalString: String? = "hello"
+optionalString!
+optionalString = nil
