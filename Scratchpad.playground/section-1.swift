449cd6df377b4576c76c6bcd100a95d0d844af27
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-20 21:49:01 +0900

    Add Adding a Pop of Color Step

--- Scratchpad.playground/section-1.swift
+++ Scratchpad.playground/section-1.swift
@@ -1,6 +1,6 @@
 // Playground - noun: a place where people can play
 
-import Cocoa
+import UIKit
 
 var str = "Hello, playground"
 
@@ -25,3 +25,7 @@ optionalString = nil
 
 // Random Number Generation
 var randomNumber = Int(arc4random_uniform(10))
+
+// UIColor
+var redColor = UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0)
+let colorArray = [redColor]
